831dacaf4dc20824cb3e4635a9f9874c2f8d946e
Archana Kumari Sah <saharchanakumari2@gmail.com>
2024-12-11 21:05:25 +0545

    modified login page

--- login.php
+++ login.php
@@ -106,7 +106,7 @@ button:hover{
 					<label for="chk" aria-hidden="true">Sign up</label>
 					<input type="text" name="txt" placeholder="User name" required="">
 					<input type="email" name="email" placeholder="Email" required="">
-          <input type="number" name="broj" placeholder="BrojTelefona" required="">
+          
 					<input type="password" name="pswd" placeholder="Password" required="">
 					<button>Sign up</button>
 				</form>
